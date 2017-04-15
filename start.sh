dc25f2dc6e2b78071e88454c940af0f59b99ed96
Resheph <resheph@localhost>
2017-04-15 11:33:43 +0200

    Added lowered tx power on AP.

--- start.sh
+++ start.sh
@@ -9,7 +9,9 @@ if [ -z "$1" ]; then INTERFACE="wlan1"; else INTERFACE=$1; fi
 echo "[!] Run this script using start.sh <wireless interface>"
 echo "[*] Displaying RFC reader status."
 nfc-list
-echo "[*] Setting IP address on $INTERFACE."
+echo "[*] Setting IP address on $INTERFACE etc."
+ifconfig $INTERFACE down
+iwconfig $INTERFACE txpower 12
 ifconfig $INTERFACE 169.254.0.1 netmask 255.255.0.0
 ifconfig $INTERFACE up
 echo "[*] Restarting Apache 2 to make sure it will listen on $INTERFACE."
