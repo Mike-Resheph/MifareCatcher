1c22900f7d036a50abfeb91f8c4695bb2b950bc6
Resheph <resheph@localhost>
2017-04-15 08:43:31 +0200

    Fixed a bug in start.sh

--- start.sh
+++ start.sh
@@ -5,16 +5,17 @@
 # Usage: start.sh <interface>
 # ex. start.sh wlan1
 
+if [ -z "$1" ]; then INTERFACE="wlan1"; else INTERFACE=$1; fi
 echo "[!] Run this script using start.sh <wireless interface>"
 echo "[*] Displaying RFC reader status."
 nfc-list
-echo "[*] Setting IP address on $1."
-ifconfig $1 192.168.0.1 255.255.255.0
-ifconfig $1 up
-echo "[*] Restarting Apache 2 to make sure it will listen on $1."
-systemctl restart apache2
+echo "[*] Setting IP address on $INTERFACE."
+ifconfig $INTERFACE 192.168.0.1 netmask 255.255.255.0
+ifconfig $INTERFACE up
+echo "[*] Restarting Apache 2 to make sure it will listen on $INTERFACE."
+systemctl restart apache2.service
 echo "[*] Restarting/starting the DHCP server."
-udhcpd
+systemctl restart udhcpd.service
 echo "[*] Starting the AP."
 hostapd wpa2.conf &
 echo "[*] Starting the capture using MifareCatcher."
